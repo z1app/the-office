bef0d723d1863e71d784c6338c2a7188865dde9a
Luciano Franca <lucianopf@outlook.com>
2019-08-03 23:36:06 -0300

    routes: add rooms routes

--- src/components/Room.svelte
+++ src/components/Room.svelte
@@ -8,7 +8,10 @@
     setUserProfile,
   } from '../services/local'
 
-  import { arrayrify } from '../services/helpers'
+  import {
+    arrayrify,
+    slugify
+  } from '../services/helpers'
 
   export let name
   export let users
@@ -16,6 +19,14 @@
   export let pinnedRoom
   export let id
   export let globalyPinnedRoom
+
+  function updateRoomPath (name) {
+    if (!name) {
+      history.pushState({}, 'Index', '#/')
+    } else {
+      history.pushState({}, name, `#/rooms/${slugify(name)}`)
+    }
+  }
 
   function enterRoom () {
     const userProfile = getUserProfile()
@@ -35,39 +46,66 @@
           activeRoom: id,
           activeRoomName: name,
         })
-        return Users.update(
-          userProfile.id,
-          {
-            activeRoom: id,
-            activeRoomName: name,
-          }
-        )
+
+        return updateRoomPath(name)
       })
-      .then(() => Rooms.update(`${id}/users/${userProfile.id}`, userProfile))
+      .then(() => {
+        return Promise.all([
+          Users.update(
+            userProfile.id,
+            {
+              activeRoom: id,
+              activeRoomName: name,
+            }
+          ),
+          Rooms.update(
+            `${id}/users/${userProfile.id}`,
+            userProfile
+          )
+        ])
+      })
   }
 
   function leaveRoom () {
     const userProfile = getUserProfile()
-    return Rooms.delete(`${id}/users/${userProfile.id}`)
-      .then(() => {
-        setUserProfile({
-          ...userProfile,
-          activeRoom: undefined,
-          activeRoomName: undefined,
-        })
-        return Users.update(
-          userProfile.id,
-          {
-            activeRoom: null,
-            activeRoomName: null,
-          }
-        )
-      })
+
+    setUserProfile({
+      ...userProfile,
+      activeRoom: undefined,
+      activeRoomName: undefined,
+    })
+    updateRoomPath()
+
+    return Promise.all([
+      Rooms.delete(`${id}/users/${userProfile.id}`),
+      Users.update(
+        userProfile.id,
+        {
+          activeRoom: null,
+          activeRoomName: null,
+        }
+      )
+    ])
   }
 
   function togglePin () {
     const uid = getUserId()
     Users.set(`${uid}/pinnedRooms/${id}`, !pinnedRoom)
+  }
+
+  function getCurrentPath () {
+    const { hash } = window.location
+    if (!hash || !hash.length) {
+      return false
+    }
+
+    return hash.split('/').pop()
+  }
+
+  const pathRoom = getCurrentPath()
+
+  if (pathRoom === slugify(name)) {
+    enterRoom()
   }
 </script>
 
--- src/services/helpers.js
+++ src/services/helpers.js
@@ -31,8 +31,30 @@ function unArrayrify (arr) {
   }, {})
 }
 
+function slugify (string) {
+  return string.toLowerCase()
+    .replace(/[àÀáÁâÂãäÄÅåª]+/g, 'a')
+    .replace(/[èÈéÉêÊëË]+/g, 'e')
+    .replace(/[ìÌíÍîÎïÏ]+/g, 'i')
+    .replace(/[òÒóÓôÔõÕöÖº]+/g, 'o')
+    .replace(/[ùÙúÚûÛüÜ]+/g, 'u')
+    .replace(/[ýÝÿŸ]+/g, 'y')
+    .replace(/[ñÑ]+/g, 'n')
+    .replace(/[çÇ]+/g, 'c')
+    .replace(/[ß]+/g, 'ss')
+    .replace(/[Ææ]+/g, 'ae')
+    .replace(/[Øøœ]+/g, 'oe')
+    .replace(/[%]+/g, 'pct')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
+
 module.exports = {
   arrayrify,
+  slugify,
   safeArrayrify,
   unArrayrify,
 }
