0f86f9afbefbdfe1bfa557fdf5e4fd8ba1cb4c30
lucianopf <lucianopf@outlook.com>
2019-11-18 09:23:00 -0300

    manage: add the delete rooms option

---
+++ src/components/CreateRoom.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { Rooms } from '../models'
+  import { getUserId } from '../services/local'
+  import { sideBar } from '../services/store'
+
+  let roomName = ''
+  const userId = getUserId()
+
+  function createRoom () {
+    if (!roomName || roomName.length < 3) {
+      window.alert('O nome de uma nova sala deve ter no mínimo 3 caracteres')
+      return false
+    }
+
+    return Rooms.create({
+      name: roomName,
+      createdBy: userId,
+    })
+      .then(() => {
+        roomName = ''
+        return sideBar.update(() => 'list-rooms')
+      })
+  }
+</script>
+
+<style>
+  .actions {
+    padding-top: 15px;
+  }
+</style>
+
+<div class="nes-container with-title is-centered">
+  <p class="title">criar sala</p>
+  <div class="nes-field">
+    <label for="name_field">nome</label>
+    <input
+      type="text"
+      id="name_field"
+      class="nes-input"
+      bind:value={roomName}
+    />
+  </div>
+  <div class='actions'>
+    <button
+      type="button"
+      class="nes-btn is-primary"
+      on:click={createRoom}
+    >
+      criar
+    </button>
+  </div>
+</div>
--- src/containers/Manage.svelte
+++ src/containers/Manage.svelte
@@ -1,54 +1,25 @@
 <script>
+  import CreateRoom from '../components/CreateRoom.svelte'
+  import ManageRoom from '../components/ManageRoom.svelte'
+
   import { Rooms } from '../models'
   import { getUserId } from '../services/local'
-  import { sideBar } from '../services/store'
 
-  let roomName = ''
+  let myRooms = []
+  const userId = getUserId()
 
-  function createRoom () {
-    if (!roomName || roomName.length < 3) {
-      window.alert('O nome de uma nova sala deve ter no mínimo 3 caracteres')
-      return false
+  Rooms.watch(
+    '/',
+    true,
+    (allRooms = []) => {
+      myRooms = allRooms.filter(room => room.createdBy === userId)
     }
-
-    const uid = getUserId()
-    return Rooms.create({
-      name: roomName,
-      createdBy: uid,
-    })
-      .then(() => {
-        roomName = ''
-        return sideBar.update(() => 'list-rooms')
-      })
-  }
+  )
 </script>
 
-<style>
-  .actions {
-    padding-top: 15px;
-  }
-</style>
-
 <div>
-  <div class="nes-container with-title is-centered">
-    <p class="title">criar sala</p>
-    <div class="nes-field">
-      <label for="name_field">nome</label>
-      <input
-        type="text"
-        id="name_field"
-        class="nes-input"
-        bind:value={roomName}
-      />
-    </div>
-    <div class='actions'>
-      <button
-        type="button"
-        class="nes-btn is-primary"
-        on:click={createRoom}
-      >
-        criar
-      </button>
-    </div>
-  </div>
+  <CreateRoom />
+  {#each myRooms as room}
+    <ManageRoom {room} />
+  {/each}
 </div>
